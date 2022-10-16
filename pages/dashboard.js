15c3a6d40bbc44dd8dc09fdcf5bffa52a9ab6fda
unknown <fahimismail75.scc@gmail.com>
2022-10-16 21:12:30 +0600

    dash

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,3 +1,4 @@
+import { Logout } from '@mui/icons-material'
 import { Avatar } from '@mui/material'
 import { useRouter } from 'next/router'
 import React, { useContext, useEffect } from 'react'
@@ -6,23 +7,28 @@ import { Store } from '../utils/store'
 
 const Dash = () => {
 
+    const router = useRouter ()
+
     const {state, dispatch} = useContext(Store)
     console.log(state.userInfo)
 
     useEffect(()=> {
-        !state.userInfo&&useRouter().push("/login")
+        !state.userInfo&&router.push('/login')
     }, [state.userInfo])
     
     return (
     <Layout>
         <div className="flex">
-            <div className="w-[20rem] bg-sky-300 min-h-screen mt-24">
+            <div className="w-[20rem] bg-slate-800 min-h-screen mt-24">
                 <div className="p-10 flex justify-evenly items-center">
                     <Avatar />
                     <p>{state.userInfo.username}</p>
                 </div>
+                <div className="p-10 flex justify-evenly items-center">
+                    <p><Logout/> Log out</p>
+                </div>
             </div>
-            <div className="bg-white min-h-screen mt-24 w-screen">
+            <div className="min-h-screen mt-24 w-screen bg-slate-600">
                 <div>
 
                 </div>
