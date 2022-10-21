efeceb39296f69ff2a985a413fe59a51899cf89d
Md Farhad Jaman <www.farhadjaman11@gmail.com>
2022-10-21 06:44:00 +0600

    Added New Header

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../styles/globals.css'
 import { SnackbarProvider } from 'notistack'
 import { StoreProvider } from '../utils/store'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const use = async () => {
+      (await import('tw-elements')).default;
+    };
+    use();
+  }, []);
 
   return (<SnackbarProvider anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
-              <StoreProvider>
-                <Component {...pageProps} />
-              </StoreProvider>
-          </SnackbarProvider>)
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  </SnackbarProvider>)
 }
 
 export default MyApp
